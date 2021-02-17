688bfc1e9976ecd5ae484271ba4327921ea31714
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-02-17 11:51:32 +0100

    adding with id!

--- my-app/src/components/addItems/addItems.js
+++ my-app/src/components/addItems/addItems.js
@@ -2,11 +2,26 @@ import React from "react";
 import "./AddItems.css";
 
 class AddItems extends React.Component {
-  state = {};
+  state = {
+    name: "",
+    age: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: [e.target.value],
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Enter name..."
