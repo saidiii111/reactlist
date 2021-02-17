491f608a6bff4157df1c148359a3b352e63f8f2d
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-02-17 11:04:32 +0100

    Show no Items Message!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import './App.css';
-import TodoItems from './components/TodoItems/TodoItems';
-import AddItems from './components/AddItems/AddItems';
-
+import React from "react";
+import "./App.css";
+import TodoItems from "./components/TodoItems/TodoItems";
+import AddItems from "./components/AddItems/AddItems";
 
 class App extends React.Component {
   state = {
     items: [
-      { id: 1, name: 'Fayad', age: 31 },
-      { id: 2, name: 'Rabee', age: 32 },
-      { id: 3, name: 'Majed', age: 28 }
-    ]
-  }
+      { id: 1, name: "Fayad", age: 31 },
+      { id: 2, name: "Rabee", age: 32 },
+      { id: 3, name: "Majed", age: 28 },
+    ],
+  };
 
   deletItem = (id) => {
-    let items = this.state.items.filter(item => {
-      return item.id !== id
-    })
-   this.setState({items})
-  }
+    let items = this.state.items.filter((item) => {
+      return item.id !== id;
+    });
+    this.setState({ items });
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="App">
-      <header className="App-header">
+        <header className="App-header">
           testing
-             <TodoItems items={this.state.items} deletItem={this.deletItem} />
+          <TodoItems items={this.state.items} deletItem={this.deletItem} />
           <AddItems />
-      </header>
-    </div>
-  );
-   
+        </header>
+      </div>
+    );
   }
 }
- 
+
 export default App;
-
-
--- my-app/src/components/todoItems/todoItems.js
+++ my-app/src/components/todoItems/todoItems.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import './TodoItems.css'
+import React from "react";
+import "./TodoItems.css";
 
 const TodoItems = (props) => {
-    const { items, deletItem } = props;
-    const ListItems = items.map(item => {
-        return (
-            <div key={item.id}>
-                <span>{item.neme}</span>
-                <span>{item.age}</span>
-                <span onClick={() => deletItem(item.id)}>&times;</span>
-            </div>
-        )
-    })
-    return (
-        <div className="ListItems">
-            <div>
-                <span>Name</span>
-                <span>Age</span>
-                <span>Action</span>
-            </div>
-            {ListItems}
+  const { items, deletItem } = props;
+  let length = items.length;
+  const ListItems = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id}>
+          <span>{item.neme}</span>
+          <span>{item.age}</span>
+          <span onClick={() => deletItem(item.id)}>&times;</span>
         </div>
-    );
-  }
-
+      );
+    })
+  ) : (
+    <span>there is no items to show</span>
+  );
+  return (
+    <div className="ListItems">
+      <div>
+        <span>Name</span>
+        <span>Age</span>
+        <span>Action</span>
+      </div>
+      {ListItems}
+    </div>
+  );
+};
 
 export default TodoItems;
