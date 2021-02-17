984ef026217ccdd78830611169afda5195a199c4
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-02-17 11:31:29 +0100

    adding form to additems

--- my-app/src/components/addItems/addItems.js
+++ my-app/src/components/addItems/addItems.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import './AddItems.css'
+import React from "react";
+import "./AddItems.css";
 
 class AddItems extends React.Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                Add Item
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter name..."
+            id="name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Enter age..."
+            id="age"
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Adding" />
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default AddItems;
